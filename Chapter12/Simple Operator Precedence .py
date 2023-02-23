7ab14475bd33e7fab18e66e12ee680211bbce0d7
Bhagyashri-kitey24 <bhagyashrikitey924@gmail.com>
2023-02-23 18:21:30 +0530

    commit

---
+++ Chapter12/Simple Operator Precedence .py
@@ -0,0 +1,9 @@
+a, b, c, d = 2, 3, 5, 7
+a ** (b + c) # parentheses
+a * b ** c # exponent: same as `a * (b ** c)`
+a + b * c / d # multiplication / division: same as `a + (b * c / d)`
+#Extras: mathematical rules hold, but not always:
+300 / 300 * 200
+300 * 200 / 300
+1e300 / 1e300 * 1e200
+1e300 * 1e200 / 1e300
