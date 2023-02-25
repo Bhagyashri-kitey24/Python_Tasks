916cc05d588092b82927a081ca81fd7afb018ddc
Bhagyashri-kitey24 <bhagyashrikitey924@gmail.com>
2023-02-25 11:13:09 +0530

    sET

---
+++ Chapter8/Get the unique elements of a list.py
@@ -0,0 +1,12 @@
+# The best way to get the unique elements from a list is to turn it into a set:
+
+girls_name = ["Bhagi", "Sanju", "Appu", "Dipa", "Appu","Ishu","Bhagi","Dami","Sanju" ]
+a = set(girls_name) #it will give only unic names
+print(a)
+print()
+
+b=list(girls_name) #it will print all elements
+print(b)
+
+c=list(set(girls_name)) # #it will give only unic names
+print(c)
