7932d44176a3fb3274b3f3d3e7f7462ba8780cc1
Bhagyashri-kitey24 <bhagyashrikitey924@gmail.com>
2023-02-26 00:19:53 +0530

    Conditionals

---
+++ Chapter 14/Boolean Logic Expressions.py
@@ -0,0 +1,17 @@
+print("-----logic for or-------")
+a=True
+b=False
+if a or b:
+    print("I am in if block")
+else:
+    print("I am in else block")
+print("I will execute always")
+
+print("-----logic for and-------")
+a=False
+b=True
+if a and b:
+    print("I am in if block")
+else:
+    print("I am in else block")
+print("I will execute always")
---
+++ Chapter 14/if, elif, and else.py
@@ -0,0 +1,14 @@
+#Start
+n=int(input("Enter the salary"))
+if (n>100000):
+    print("The profession of employee is database administrator")
+elif(n>=75000 and n<=100000):
+    print("The employee is software developer")
+elif(n>=60000 and n<=75000):
+    print("The employee is software tester")
+elif(n>=40000 and n<=60000):
+    print("The employee is software designer") 
+else:
+    print("The employee is hardware designer")   
+ 
+ #End   
