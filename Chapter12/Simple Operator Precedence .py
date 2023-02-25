e2661ea2796aae6cda698209fe834027587926d8
Bhagyashri-kitey24 <bhagyashrikitey924@gmail.com>
2023-02-25 18:04:59 +0530

    Operator Precedence

--- Chapter12/Simple Operator Precedence .py
+++ Chapter12/Simple Operator Precedence .py
@@ -1,9 +1,7 @@
 a, b, c, d = 2, 3, 5, 7
-a ** (b + c) # parentheses
-a * b ** c # exponent: same as `a * (b ** c)`
-a + b * c / d # multiplication / division: same as `a + (b * c / d)`
-#Extras: mathematical rules hold, but not always:
-300 / 300 * 200
-300 * 200 / 300
-1e300 / 1e300 * 1e200
-1e300 * 1e200 / 1e300       
+sum1=a ** (b + c) # parentheses
+sum2=a * b ** c # exponent: same as `a * (b ** c)`
+sum3=a + b * c / d # multiplication / division: same as `a + (b * c / d)`
+print(sum1) #256
+print(sum2) #486
+print(sum3) #4.142857142857142
