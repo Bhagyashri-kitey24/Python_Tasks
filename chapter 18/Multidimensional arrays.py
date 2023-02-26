4d9af52e4ff0f2439225dcffd8c1da39efabc5a3
Bhagyashri-kitey24 <bhagyashrikitey924@gmail.com>
2023-02-26 12:53:27 +0530

    Multidimensional arrays

---
+++ chapter 18/Multidimensional arrays.py
@@ -0,0 +1,19 @@
+print("----list in list i.e.->2D") 
+lst = [[1,2,3], 
+         [4,5,6], 
+         [7,8,9]]
+
+print(lst[0]) #[1, 2, 3]
+print(lst[0][0])#1
+print(lst[0][1])#2
+print(lst[0][2])#3
+
+print(lst[1]) #[4, 5, 6]
+print(lst[1][0])#4
+print(lst[1][1])#5
+print(lst[1][2])#6
+
+print(list1[2]) #[7, 8, 9]
+print(list1[2][0])#7
+print(list1[2][1])#8
+print(list1[2][2])#9
